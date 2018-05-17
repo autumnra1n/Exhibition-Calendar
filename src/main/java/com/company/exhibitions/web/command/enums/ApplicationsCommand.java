a57ed3da61726bc34c6f20729c3b5c142371d4b0
i5-3570k\Михаил <sahnyuk.misha@gmail.com>
2018-05-17 18:31:43 +0300

    project updated

--- src/main/java/com/company/exhibitions/web/command/enums/ApplicationsCommand.java
+++
@@ -1,6 +0,0 @@
-package com.company.exhibitions.web.command.enums;
-
-public enum ApplicationsCommand {
-
-    VERIFY_LOGIN
-}
--- src/main/java/com/company/exhibitions/web/command/enums/RolesCommand.java
+++
@@ -1,9 +0,0 @@
-package com.company.exhibitions.web.command.enums;
-
-public enum RolesCommand {
-    INSERT_ROLE,
-    UPDATE_ROLE,
-    DELETE_ROLE,
-    FIND_ALL,
-    FIND_ROLE_BY_ID
-}
